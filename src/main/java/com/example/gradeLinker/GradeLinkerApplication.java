42f7dce54e13ea03e4a716254409e094bbec4546
ArtemPika <orelartem228@gmail.com>
2023-11-28 16:51:06 +0200

    Initial commit

---
+++ src/main/java/com/example/gradeLinker/GradeLinkerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradeLinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeLinkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeLinkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gradeLinker/GradeLinkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradeLinker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeLinkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
